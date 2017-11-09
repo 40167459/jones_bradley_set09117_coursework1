809ab53489bc72fabe829d3f2d88815041b4e1b5
40167459 <jonesbradley12@yahoo.co.uk>
2017-11-09 12:27:54 +0000

    Added error handling to the move function

--- draughts.py
+++ draughts.py
@@ -3,8 +3,8 @@
 #Bradley Jones
 #27/10/2017
 
-GRID_HEIGHT = 10
-GRID_WIDTH = 10
+grid_height = 8
+grid_width = 8
 #White Draught
 WD = "%"
 #White King
@@ -19,23 +19,35 @@ BK = '$'
 
 #END OF CONSTANTS SECTION
 
+#def main():
+   
+
 #Defines the game board
 """Initialize the new game grid"""
-grid = [[EC, BD, EC, BD, EC, BD, EC, BD, EC, BD],
-        [BD, EC, BD, EC, BD, EC, BD, EC, BD, EC],
-        [EC, BD, EC, BD, EC, BD, EC, BD, EC, BD],
-        [BD, EC, BD, EC, BD, EC, BD, EC, BD, EC],
-        [EC, EC, EC, EC, EC, EC, EC, EC, EC, EC],
-        [EC, EC, EC, EC, EC, EC, EC, EC, EC, EC],
-        [EC, WD, EC, WD, EC, WD, EC, WD, EC, WD],
-        [WD, EC, WD, EC, WD, EC, WD, EC, WD, EC],
-        [EC, WD, EC, WD, EC, WD, EC, WD, EC, WD],
-        [WD, EC, WD, EC, WD, EC, WD, EC, WD, EC]]
+grid = [[BD, EC, BD, EC, BD, EC, BD, EC],
+        [EC, BD, EC, BD, EC, BD, EC, BD],
+        [BD, EC, BD, EC, BD, EC, BD, EC],
+        [EC, EC, EC, EC, EC, EC, EC, EC],
+        [EC, EC, EC, EC, EC, EC, EC, EC],
+        [EC, WD, EC, WD, EC, WD, EC, WD],
+        [WD, EC, WD, EC, WD, EC, WD, EC],
+        [EC, WD, EC, WD, EC, WD, EC, WD]]
 
-
+#Prints the board
 def print_board(grid):
     acc = 0
-    print(" 0 1 2 3 4 5 6 7 8 9 10 ")
+    print(" 0    1    2    3    4    5    6    7   ")
     for row in grid:
         print(acc, row)
         acc = acc + 1
+
+def move():
+    src_x = int(input("Enter an x coord for where to move from"))
+    src_y = int(input("Enter a y coord for where to move from"))
+    dst_x = int(input("Enter an x coord for where to move to"))
+    dst_x = int(input("Enter a y coord for where to move to"))
+    if grid[src_y][src_x] == EC:
+        print("Empty cell selected try again")
+        return move()
+
+
